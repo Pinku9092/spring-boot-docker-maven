fba17b03d06c6f7a96314962970e81291dd99c73
Pinku <kpinku32@gmail.com>
2024-04-25 16:08:09 +0530

    initial commit

---
+++ src/main/java/com/pinku/spring/boot/docker/maven/demo/SpringBootDockerMavenApplication.java
@@ -0,0 +1,13 @@
+package com.pinku.spring.boot.docker.maven.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pinku/spring/boot/docker/maven/demo/SpringBootDockerMavenApplicationTests.java
@@ -0,0 +1,14 @@
+package com.pinku.spring.boot.docker.maven.demo;
+/*
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
